b1c88fcd74db618c29ec19f30bb34715049d3bf0
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-04-25 22:03:46 +0200

    init project with user model

---
+++ authentication/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, blank=False, unique=True, null=False)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(auto_now_add=True)
---
+++ taskBackend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('auth/login'),
+    # path('auth/sigup'),
+    # path('task/'),
+    # path('task/add/'),
+    # path('task/edit'),
+    # path('task/delete/')
+]
