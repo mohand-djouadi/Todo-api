6fa8dc6e8203b4688cdc908fd5583dfe51f8db1f
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-04-26 00:10:31 +0200

    add signup login

--- taskBackend/settings.py
+++ taskBackend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'taskManagement'
+    'taskManagement',
 ]
 
 MIDDLEWARE = [
--- taskManagement/models.py
+++ taskManagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Task:
+    title = models.CharField(max_length=80, blank=False, null=False)
+    taskDate = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    location = models.CharField(max_length=80, blank=True, null=True)
+    done = models.BooleanField(default=False, blank=False, null=False)
+    description = models.CharField(max_length=5000, blank=True, null=True)
