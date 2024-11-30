e8a8892e352f34bb61839f2fdf1059c32f1e009c
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-11-30 23:27:48 +0100

    add render root

--- taskBackend/settings.py
+++ taskBackend/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'assets'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
