6204bbfacb40e38c853790d516025f1f03366d13
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-12-03 21:40:39 +0100

    hide var envs

--- taskBackend/settings.py
+++ taskBackend/settings.py
@@ -11,30 +11,41 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n^^hvv^z=vj#1jz*+f38yy&khb8u-mnpkj%&0cc5*-irqqc55u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['todo-api-eeh0.onrender.com']
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-    'https://todo-api-eeh0.onrender.com'
+ALLOWED_HOSTS = [
+    config('LOCAL_FRONT_HOST'),
+    config('HTTPS_RENDER_SERVER'),
+    config('HTTP_RENDER_SERVER'),
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://todo-api-eeh0.onrender.com', 'http://todo-api-eeh0.onrender.com']
+CORS_ALLOWED_ORIGINS = [
+    config('LOCAL_FRONT_HOST'),
+    config('HTTPS_RENDER_SERVER'),
+    config('HTTP_RENDER_SERVER'),
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    config('HTTPS_RENDER_SERVER'),
+    config('HTTP_RENDER_SERVER'),
+    config('LOCAL_FRONT_HOST')
+]
 
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 CSRF_COOKIE_SECURE = True
@@ -92,6 +103,7 @@ WSGI_APPLICATION = 'taskBackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': dj_database_url.config(
