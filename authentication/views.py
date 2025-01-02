9db17599213eb1a4e7a878e67b2d340b5b063db3
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2025-01-02 19:04:44 +0100

    add get and validate otp views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
+import datetime
+from random import randint
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+from django.utils.timezone import localtime
+
+from django.conf import settings
 from django.contrib.auth import authenticate,login,logout
 from django.db.models import Q
 from django.http import JsonResponse
@@ -82,6 +92,73 @@ def signUp(request):
             return JsonResponse({'error': error}, status=400)
     else:
         return JsonResponse({'error': 'HTTP method not allowed'}, status=405)
+
+
+
+def get_OTP(request):
+    if request.method == 'GET':
+        email = request.GET.get('email')
+        if not email:
+            return JsonResponse({'error':'email parameter is required'}, status=400)
+        else:
+            try:
+                otp = ''.join([str(randint(0, 9)) for i in range(6)])
+                user = User.objects.get(email=email)
+                user.otp = otp
+                user.otp_expiry = timezone.now() + datetime.timedelta(minutes=10)
+                user.save()
+                subject = 'Votre code de vérification OTP'
+                plain_message = 'Votre code de vérification est : ' + otp
+                html_message = render_to_string('email.html', {'user': user, 'otp': otp})
+                send_mail(
+                    subject, # Message en texte brut
+                    plain_message,
+                    settings.EMAIL_HOST_USER,  # L'adresse email de l'expéditeur
+                    [email],  # L'adresse email du destinataire
+                    html_message=html_message,  # Message HTML
+                )
+                return JsonResponse({'message': 'OTP sent successfully'}, status=200)
+            except User.DoesNotExist:
+                return JsonResponse({'error':'email not exist'}, error=404)
+    else:
+        return JsonResponse({'error':'HTTP method not allowed'}, status=405)
+
+
+def validate_otp(request):
+    if request.method == 'POST':
+        try:
+            email = request.GET.get('email')
+            otp = json.loads(request.body).get('otp', '')
+            if otp != '' or email:
+                try:
+                    user = User.objects.get(email=email)
+                except User.DoesNotExist:
+                    return JsonResponse({'error': 'User not found'}, status=404)
+                print('user', user.username)
+                print("now", localtime(timezone.now()))
+                print("otp expiry",  localtime(user.otp_expiry))
+                print("is otp not expired ",localtime(timezone.now()) < localtime(user.otp_expiry))
+                if localtime(timezone.now()) < localtime(user.otp_expiry):
+                    if user.otp == otp:
+                        user.otp = None
+                        user.otp_expiry = None
+                        print('before save')
+                        user.save()
+                        print('after save')
+                        return JsonResponse({'message':'otp verified succesfully'}, status=200)
+                    else:
+                        return JsonResponse({'error':'otp is not valide'}, status=400)
+                else:
+                    return JsonResponse({'error':'otp is expired'}, status=400)
+            else:
+                return JsonResponse({'error':'otp and email is required'}, status=404)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'invalid data format'}, status=400)
+        except ValidationError as e:
+            error = dict(e)
+            return JsonResponse({'error': error}, status=400)
+    else:
+        return JsonResponse({'error','HTTP method not allowed'}, status=405)
 
 
 @login_required
