a19e9c49711a9510a8b57a870b2ccb0a2f67bf18
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2025-01-03 14:11:51 +0100

    add email template for otp

--- taskBackend/settings.py
+++ taskBackend/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'taskBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
